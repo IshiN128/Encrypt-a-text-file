0592e7ab6428de65eca041951cd08fc6376b61ac
IshiN128 <ishingit@protonmail.com>
2021-10-16 16:15:54 +0530

    Add project files.

---
+++ encrypt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace encrypt
+{
+    public class Encrypter
+    {
+        static void Main(string[] args)
+        {
+            string inputFilename = "input.txt";
+            string outputFilename = "output.txt";
+
+            string contents = File.ReadAllText(inputFilename);
+
+            string cipher = string.Empty;
+            byte key = 13;
+            foreach (char letter in contents)
+            {
+                cipher += (char)(letter + key);
+            }
+
+            File.WriteAllText(outputFilename, cipher);
+        }
+    }
+}
